e9b6bc04b26b32ce73e458c3617137961f9dff82
niklasbec <niklasbeckerr@gmail.com>
2020-01-17 11:09:56 +0100

    react app done delete is needs hard reload not sure what went wrong

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState([])
+  const [formValues, setFormValues] = useState({
+    name: '',
+    description: ''
+  })
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/projects")
+      .then(response => {
+        setData(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
+
+  const newPost = (e) => {
+    e.preventDefault();
+    
+    axios.post("http://localhost:4000/api/projects", formValues)
+      .then( res => {
+        setData([...data, res.data.projectInfo])
+        alert('Success, scroll down to see project')
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  const deletePost = (e) => {
+    e.preventDefault()
+    let id = e.target.value
+    let dataToKeep = data.filter(curr => {
+      return curr.id !== id
+    })
+    setData(dataToKeep)
+    axios.delete(`http://localhost:4000/api/projects/${id}`)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  const handleChange = event => {
+    setFormValues({...formValues,
+        [event.target.name]: event.target.value
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <h2>Add or remove Projects</h2>
+          <form onSubmit={newPost}>
+            <input type='text' name='name' onChange={handleChange} value={formValues.name} placeholder='project name' />
+            <input type='text' name='description' onChange={handleChange} value={formValues.description} placeholder='project description' />
+
+            <input type='submit' />
+
+          </form>
+        </div>
+      {data.map(curr => {
+        return(
+          <div className='project'>
+            <p>Id: {curr.id}</p>
+            <p>Project Name: {curr.name}</p>
+            <p>Project Description: {curr.description}</p>
+            <button onClick={deletePost} value={curr.id}>Delete</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
